e0128b5a7efc960bfd747bf64ed057d307763d2b
unknown <williamcarvalh0@hotmail.com>
2021-08-16 22:22:23 +0100

    first commit

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React from 'react'
+import logo from '../assets/images/ShiftMatcher.jpeg'
 
-const Home = () => {
-  return <p>Inside Home component</p>;
-};
+const Home = () => (
+  <>
+    <div className='container'>
+      <h1 className='display-3 text-center mr-auto text-secondary'>Welcome to ShiftMatcher</h1>
+      
+      
+      <div class='text-center'>
+        <img src={logo} className='img-fluid rounded mx-auto d-block p-2' alt='' />
+      </div>
 
-export default Home;
+      <blockquote class='blockquote text-center p-5'>
+        <p class='mb-0'>
+        We’re here to make hiring a little easier. Let’s give it a try.
+        
+        </p>
+        <footer class='blockquote-footer'>
+          William Carvalho <cite title='Source Title'>ShiftMatcher CEO</cite>
+        </footer>
+      </blockquote>
+    </div>
+  </>
+)
+
+export default Home
